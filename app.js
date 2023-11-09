b2c72e748420210b0d7076b6094262197bd2a445
Timothy Coleman <Timbo97@yahoo.com>
2023-11-09 15:32:12 -0500

    completed excercise 2

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
-
+let lastname = "Coleman";
+var age = "26";
 // Exercise 2
-
+const firstName = "Timothy";
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Excercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let SalePercentage= 15 ;
+let Stock = 0;
+let InStock = false;
+let SelectedSize = "m";
+
+// Excercise 5
+
+let title = "Name Of The Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456 ;
+let hasRead = true;
